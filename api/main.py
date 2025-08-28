097c0db475f8425c5db5cda8133315bb99c05be9
Your Name <shambomukherjee05@gmail.com>
2025-08-28 11:27:16 +0000

    Redirect / to /static/html/index.html for user-friendly entrypoint

--- api/main.py
+++ api/main.py
@@ -1,3 +1,8 @@
+from fastapi.responses import RedirectResponse
+# Redirect root to login page
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/static/html/index.html")
 
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
